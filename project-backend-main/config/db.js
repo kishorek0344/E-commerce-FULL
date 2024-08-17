ee0756ab75d9d65c7f37ac55c57d2d97407f6ebd
kishorek0344 <kishorekishk44@gmail.com>
2024-08-17 17:42:45 +0530

    vercel frontendui back

--- project-backend-main/config/db.js
+++ project-backend-main/config/db.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose")
 
 async function connectDB(){
     try{
-        // await mongoose.connect(process.env.MONGODB_URI)
-        await mongoose.connect('mongodb+srv://ecommercewebsiteaug12:Ecommerce_A1B2@mern.jlo0qjy.mongodb.net/MERN-Ecommerce?retryWrites=true&w=majority&appName=MERN')
+        await mongoose.connect(process.env.MONGODB_URI)
     }catch(err){
         console.log(err)
     }
