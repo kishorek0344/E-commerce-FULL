2412caad4ae4c3ee1414caf6be1fb22eddddb74e
kishorek0344 <kishorekishk44@gmail.com>
2024-08-17 17:45:11 +0530

    vercel frontendui back

--- project-backend-main/index.js
+++ project-backend-main/index.js
@@ -8,8 +8,8 @@ const router = require("./routes");
 const app = express();
 app.use(
   cors({
-    // origin: process.env.FRONTEND_URL,
-    origin: 'https://e-commerce-website-flame-six.vercel.app',
+    origin: process.env.FRONTEND_URL,
+    // origin: 'https://e-commerce-website-flame-six.vercel.app',
     credentials: true,
   })
 );
