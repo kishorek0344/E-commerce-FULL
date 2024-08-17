67053b4f7f095c3de579e8e39aea4dfbeea35b67
kishorek0344 <kishorekishk44@gmail.com>
2024-08-17 17:09:36 +0530

    vercel fronturls

--- project-backend-main/index.js
+++ project-backend-main/index.js
@@ -8,7 +8,8 @@ const router = require("./routes");
 const app = express();
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    // origin: process.env.FRONTEND_URL,
+    origin: 'https://e-commerce-website-flame-six.vercel.app',
     credentials: true,
   })
 );
